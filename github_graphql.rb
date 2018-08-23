eb1917826a97f6da201ef146e62b82ecfb938f6f
Brian Tenggren <btenggren20@gmail.com>
2018-08-23 17:31:59 -0400

    Add a cache dir

--- github_graphql.rb
+++ github_graphql.rb
@@ -2,5 +2,12 @@
 class GithubGraphql
   def self.start
     puts 'Starting'
+    setup
+  end
+
+  def self.setup
+    # if the cache file exists and is 1 day old or newer skip client initializing
+    # else
+    # recreate the cache!
   end
 end
